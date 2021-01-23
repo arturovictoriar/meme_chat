a50010eb2e13244102bed8a78e78f87599152d1e
Arturo Victoria Rincon <1231@holbertonschol.com>
2021-01-23 17:38:43 -0500

    Setup the redux saga in App

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+// Import libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+// Import modules
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './utils/registerServiceWorker';
+import store from './redux/initialization/initilize_state';
+// Render in index.html the App with the provider store
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+	);
+// Call the register Service Worker
+registerServiceWorker();
+
+
+
