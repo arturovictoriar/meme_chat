339e4ae8927f5486ff9011c0e0856f82afac7132
arturovictoriar <1231@holbertonschool.com>
2021-01-23 19:35:16 -0500

    Add ui components

---
+++ frontend/src/components/App.js
@@ -0,0 +1,24 @@
+// Import Libraries
+import React from 'react';
+// Import Modules
+import '../styles/App.css';
+import { Sidebar } from "../redux/containers/Sidebar"
+import { MessagesList } from "../redux/containers/MessagesList"
+import { AddMessage } from "../redux/containers/AddMessage"
+
+/**
+ * Main component
+ */
+const App = () => {
+  return (
+    <div id="container">
+      <Sidebar />
+      <section id="main">
+        <MessagesList />
+        <AddMessage />
+      </section>
+    </div>
+  );
+}
+// export the main component
+export default App;
