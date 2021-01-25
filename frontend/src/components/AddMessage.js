0ae4f4596ac35aa9a9dc9da05ccbacb86ae190f4
Arturo Victoria Rincon <1231@holbertonschol.com>
2021-01-25 17:30:35 -0500

    Development of youtube suggestion

--- frontend/src/components/AddMessage.js
+++ frontend/src/components/AddMessage.js
@@ -12,9 +12,7 @@ const AddMessage = (props) => {
 	let last_input = "/youtube";
 	let input;
 
-	function handleChangeYoutubeSearch(e) {
-		let ysearch = "";
-
+	const handleChangeYoutubeSearch = (e) => {
 		if (youtube_command.test(input.value) === true) {
 			input.value = prefix_search;
 			props.dispatch_mode(true);
@@ -22,28 +20,56 @@ const AddMessage = (props) => {
 		else if (props.status === true && input.value.length < prefix_search.length) {
 			input.value = last_input;
 			props.dispatch_mode(false);
+			props.dispatch_get([]);
 		}
 		else if (props.status === true && input.value.length >= prefix_search.length) {
 			props.dispatch_search(input.value.slice(prefix_search.length - 1,));
-			ysearch = props.memeSearch.replace(" ", "%20")
-			fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${ysearch}&type=video&key=AIzaSyBMJoVDJSzDGVyFTX5xZvbtBIW9KcC4ztY`).then(
-				response => response.json()).then(
-					data => {
-						props.dispatch_get(data.items);
-						console.log(data.items)
-					});
 		}
+	}
+
+	const searchYoutubeVideo = () => {
+		let ysearch = "";
+
+		ysearch = props.memeSearch.replace(" ", "%20")
+		fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${ysearch}&type=video&videoEmbeddable=true&key=AIzaSyBEwL4HbjmYB7kqJYZTaWAtXNSpk7pWPsk`).then(
+			response => {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json()
+			}).then(
+				data => {
+					props.dispatch_get(data.items);
+				}).catch(() => {
+					props.dispatch_get([{ error: "Service disabled" }]);
+				});
+	}
+
+	const sendMessages = (e) => {
+		if (e.key === 'Enter') {
+			if (props.status === true && props.memeSearch.length > 0) {
+				searchYoutubeVideo();
+			}
+			else {
+				props.dispatch_message(input.value, 'Me', 'Hoy');
+				input.value = '';
+			}
+		}
+	}
+
+	const sendVideo = (video) => {
+		const videoEmbedded = "/embeddedYoutubeVideo:" + video.id.videoId;
+		props.dispatch_mode(false);
+		props.dispatch_get([]);
+		props.dispatch_search("");
+		props.dispatch_message(videoEmbedded, 'Me', 'Hoy');
+		input.value = '';
 	}
 
 	return (
 		<section id="new-message">
 			<input
-				onKeyPress={(e) => {
-					if (e.key === 'Enter') {
-						props.dispatch_message(input.value, 'Me', 'Hoy')
-						input.value = ''
-					}
-				}}
+				onKeyPress={sendMessages}
 				onChange={handleChangeYoutubeSearch}
 				type="text"
 				ref={(node) => {
@@ -53,8 +79,11 @@ const AddMessage = (props) => {
 
 			<div id={props.status === true ? "show_suggestions" : "hide_suggestions"}>
 				<ul>
-					{props.videos.map(video => (
-						<li>Arturo</li>
+					{props.videos.map((video, i) => (
+						video.error ? <li>{video.error}</li> :
+							<li key={i} onClick={() => sendVideo(video)}>
+								<img src={video.snippet.thumbnails.default.url} alt="video"></img>
+								{video.snippet.title}</li>
 					))}
 				</ul>
 			</div>
--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -6,14 +6,30 @@ import propTypes from 'prop-types'
  * Usernames message
  * @param {Object} param0 state parameters
  */
-const Message = ({ message, author, date }) => (
-	<span>
-		<span id={author === 'Me' ? "own_message" : "entry_message"}>
-			{author} {date} : {message}
+const Message = ({ message, author, date }) => {
+	const youtube_embedded = "^/embeddedYoutubeVideo:";
+	const youtube_command = new RegExp(youtube_embedded, "gm");
+
+	return (
+		<span>
+			<span id={author === 'Me' ? "own_message" : "entry_message"}>
+				{author} {date} :
+				</span>
+			{ (youtube_command.test(message) === true) ?
+				<iframe width="300" height="200"
+					src={`https://www.youtube.com/embed/${message.slice(youtube_embedded.length - 1,)}`}
+					id={author === 'Me' ? "own_message" : "entry_message"}>
+				</iframe>
+				:
+				<span id={author === 'Me' ? "own_message" : "entry_message"}>
+					{message}
+				</span>
+			}
+			<br />
 		</span>
-		<br/>
-	</span>
-)
+	)
+
+}
 // Connect the component with redux
 Message.propTypes = {
 	message: propTypes.string.isRequired,
